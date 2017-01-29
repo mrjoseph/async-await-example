435372c70ac29cafb52c4055dff2a8285fb655b6
mrjoseph <trev_jos@hotmail.com>
2017-01-29 20:41:18 +0000

    A example show how to use async await

---
+++ index.js
@@ -0,0 +1,40 @@
+var fetch = require('node-fetch');
+
+'use strict';
+const myError = (response) => {
+	if (!response.ok) {
+		throw Error('something went wrong...');
+	}
+
+	return response;
+}
+
+const api = (str) => {
+	return new fetch(str, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Accept': 'application/json',
+			'X-Mashape-Key': 'PfWVDMPoLBmshAa5Imt1S1qVdXdwp1z73uCjsnOFhrH4V97hsa'
+		}
+	})
+}
+
+const fetchJoke = (str) => {
+	return async() => {
+		console.log('LOADING...')
+		let response = await api(str)
+			.then(myError)
+			.then(data => data.json())
+			.catch(e => e);
+
+		return (response.message)? console.log(response.message) && false : response
+
+	}
+}
+
+const foo = fetchJoke('https://api.chucknorris.io/jokes/random1');
+
+foo().then((data) => {
+	if(data) console.log(data.value);
+})
